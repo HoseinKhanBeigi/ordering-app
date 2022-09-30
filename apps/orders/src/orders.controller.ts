42247d4d317fd9dd32cd9fae3714ffc00f43e51b
hoseinkhanbeigi <hosein@hoseins-iMac.local>
2022-09-30 15:41:56 +0330

    add name

---
+++ apps/orders/src/orders.controller.ts
@@ -0,0 +1,13 @@
+import { Body, Controller, Get, Post } from '@nestjs/common';
+
+import { OrdersService } from './orders.service';
+
+@Controller()
+export class OrdersController {
+  constructor(private readonly ordersService: OrdersService) {}
+
+  @Post()
+  async createOrder(@Body request: CreateOrderRequest) {
+    return this.ordersService.createOrder(request);
+  }
+}
